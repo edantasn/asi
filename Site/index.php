d00624cc8ac4e9ce6a5101ac536278a71d8e6f8c
Eduardo Dantas <edantasn@gmail.com>
2016-01-26 17:33:01 -0300

    Master page

---
+++ Site/index.php
@@ -0,0 +1,109 @@
+<!doctype html>
+<html lang="pt-br" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+        <title>Simcon - Sistema de Marcação de Consultas</title>
+        <link rel="icon" type="image/png" href="img/favicon.png">
+        <meta name="theme-color" content="#006edd">
+        <meta name="copyright" content="© 2016 @edantasn">
+        <meta name="author" content="@edantasn">
+        <meta name="keywords" content="">
+        <meta name="description" content="">
+        <!-- Estilo -->
+        <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
+        <link type="text/css" rel="stylesheet" href="css/material.min.css">
+        <link type="text/css" rel="stylesheet" href="css/ripples.min.css">
+        <link type="text/css" rel="stylesheet" href="css/simcon.css">
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+
+        <div class="container">
+            <div class="row">
+
+                <div class="coluna col-md-2">
+
+                    <div class="logo">
+                        <a href=""><img class="img-responsive" src="img/logo.jpg" alt="Logomarca Simcon"></a>
+                    </div>
+
+                    <div class="menu">
+                        <ul>
+                            <li>Menu</li>
+                            <li>Menu</li>
+                            <li>Menu</li>
+                            <li>Menu</li>
+                            <li>Menu</li>
+                        </ul>
+                    </div>
+
+                    <footer class="rodape">
+                        © Copyright 2016 - SIMCON <br>
+                        All rights reserved.
+                    </footer>
+                </div>
+
+                <div class="content col-md-10">
+                    <div class="topo row">
+                        <div class="login col-md-6">
+                            <div class="table">
+                                <div class="cell">
+                                    Área de login
+                                </div>
+                            </div>
+                        </div>
+                        <div class="search col-md-4 pull-right">
+                            <form action="" class="">
+                                <input type="text" class="form-control" name="" id="" placeholder="Exemplo: Endodontia" required>
+                            </form>
+                        </div>
+                    </div>
+                    <header class="banner row">
+
+                    </header>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h2>Dúvidas de como funciona?</h2>
+                        </div>
+                        <div class="bloco-perfil col-sm-4 col-md-offset-2">
+                            <a class="perfil" href=""><img class="img-responsive" src="img/icone_dentista.jpg" alt="Ícone perfil"></a><br>
+                            <h4>Dentista</h4>
+                            <p>
+                                Primeiro eu queria cumprimentar os internautas. -Oi Internautas! Depois dizer que o meio ambiente é sem dúvida nenhuma uma ameaça ao desenvolvimento sustentável. E isso significa que é uma ameaça pro futuro do nosso planeta e dos nossos países. O desemprego beira 20%, ou seja, 1 em cada 4 portugueses.
+                            </p>
+                        </div>
+                        <div class="bloco-perfil col-sm-4">
+                            <a class="perfil" href=""><img class="img-responsive" src="img/icone_dentista.jpg" alt="Ícone perfil"></a><br>
+                            <h4>Paciente</h4>
+                            <p>
+                                A população ela precisa da Zona Franca de Manaus, porque na Zona franca de Manaus, não é uma zona de exportação, é uma zona para o Brasil. Portanto ela tem um objetivo, ela evita o desmatamento, que é altamente lucravito. Derrubar arvores da natureza é muito lucrativo!
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-md-10 col-md-offset-1 banner-f">
+                            <h2>Banner Informativo</h2>
+                            <p>
+                                Eu dou dinheiro pra minha filha. Eu dou dinheiro pra ela viajar, então é... é... Já vivi muito sem dinheiro, já vivi muito com dinheiro. -Jornalista: Coloca esse dinheiro na poupança que a senhora ganha R$10 mil por mês. -Dilma: O que que é R$10 mil?
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/material.min.js"></script>
+        <script src="js/ripples.min.js"></script>
+    </body>
+</html>
